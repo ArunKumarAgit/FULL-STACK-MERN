55b3e4751f7893b50e8c2bd507d1f74f2c7f2851
ARUN KUMAR A <86073278+ArunKumarAgit@users.noreply.github.com>
2022-01-29 19:36:15 +0530

    Add files via upload

---
+++ Practice set/Practice set #1/index.js
@@ -0,0 +1,73 @@
+//Quetion 1:Write a program to find maximum between two numbers.
+
+var a = 10;
+var b = 110;
+a = parseInt(a);
+b = parseInt(b);
+if (a > b) {
+    console.log("a is greater than b");
+} else if (b > a) {
+    console.log("b is greater than a");
+} else {
+    console.log("number is equal");
+}
+
+//Quetion 2:Write a program to find maximum between three numbers.
+
+var a = 10;
+var b = 40;
+var c = 10;
+a = parseInt(a);
+b = parseInt(b);
+c = parseInt(c);
+if (a > b && a > c) {
+    console.log("a is maximum");
+} else if (b > a && b > c){
+    console.log("b is maximum");
+} else if (c > a && c > b){
+    console.log("c is maximum");
+} else {
+    console.log("all are equal");
+}
+
+//Quetion 3:Write a program to check whether a number is negative, positive or zero.
+
+var a = -1;
+a = parseInt(a);
+if (a > 0) {
+    console.log("number is positive");
+} else if (a < 0) {
+    console.log("number is negative");
+} else {
+    console.log("number is equal");
+}
+*/
+//Quetion 4:Write a program to check whether a number is divisible by 5 and 11 or not.
+
+var a = 22;
+a = parseInt(a);
+if (a % 5 == 0 || a % 11 == 0 ) {
+    console.log("number is divisible");
+} else {
+    console.log("number is not divisible");
+}
+
+
+//Quetion 4:Write a program to check whether a number is even or odd.
+
+var a = -7;
+a = parseInt(a);
+if (a % 2 == 0) {
+    console.log("number is even");
+} else {
+    console.log("number is odd");
+}
+
+
+
+
+
+
+
+
+
