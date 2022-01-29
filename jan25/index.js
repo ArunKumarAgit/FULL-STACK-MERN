fd2974137cb7dd1f53ac257d0f5ba5ffdc411db0
ARUN KUMAR A <86073278+ArunKumarAgit@users.noreply.github.com>
2022-01-29 18:45:54 +0530

    Add files via upload

---
+++ jan25/index.js
@@ -0,0 +1,38 @@
+//assignment 1:
+// Write a program to check whether the number is greater than or equal to 10 or not.
+
+var a = 10;
+
+if(a < 10) {
+    console.log("number is lesser");
+} 
+else if(a > 10) {
+    console.log("number is greater");
+}
+else {
+    console.log("number is equal");
+}
+
+output
+------------
+number is lesser
+--------------------------------------
+//assignment 2:
+//take 3 numbers and write a program to find maximum of those 3 numbers.
+var a = 18;
+var b = 10;
+var c = 56;
+if(a>b && a>c) {
+    console.log("a is maximum");
+} else if(b>a && b>c) {
+    console.log("b is maximum");
+} else {
+    console.log("c is maximum");
+}
+
+output
+------------
+c is maximum
+--------------------------------------
+
+
