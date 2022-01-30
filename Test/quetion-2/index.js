0979911ad5ef6d2c66fdda5145fabde888e4e2f4
ARUN KUMAR A <86073278+ArunKumarAgit@users.noreply.github.com>
2022-01-30 18:44:06 +0530

    Update index.js

--- Test/quetion-2/index.js
+++ Test/quetion-2/index.js
@@ -1,21 +1,22 @@
 //Write a program to print all prime numbers in range 1 to n.take a input from the user
 
-var num = prompt("Enter a number");
-num = parseInt(num);
-var count = 0;
-if(isNaN(num)) {
-    alert("Please enter the number");
+var n = prompt("Enter a number:");
+n = parseInt(n);
+if(isNaN(n) || n <= 1) {
+    alert("enter valid number");
 }
-    for(var i = 1; i <= num; i++) {
-    if(num % i == 0) {
-    count++;
+for (j = 2; j <= n; j++) {
+  let p = 1;
+  let i = 2;
+
+  while (i < j) {
+    if (parseInt(j % i) == 0) {
+      p = 0;
+      break;
     }
+    i++;
+  }
+  if (p == 1) {
+    console.log(j);
+  }
 }
-    if(count == 2) {
-        alert(num + " is prime a number");
-     }
-     else {
-        alert(num + " is not a prime number");
-      }
-
-
