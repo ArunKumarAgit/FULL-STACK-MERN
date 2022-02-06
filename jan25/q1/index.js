604337d0b111437bed04fd5e1d9743f7d5ef3a3a
ARUN KUMAR A <86073278+ArunKumarAgit@users.noreply.github.com>
2022-02-06 16:57:50 +0530

    Update index.js

--- jan25/q1/index.js
+++ jan25/q1/index.js
@@ -1,5 +1,17 @@
-var obj = {
-    "first_name": "arun",
-    "last_name": "kumar",
+//assignment 1:
+// Write a program to check whether the number is greater than or equal to 10 or not.
+
+var a = 10;
+
+if(a < 10) {
+    console.log("number is lesser");
+} 
+else if(a > 10) {
+    console.log("number is greater");
 }
-console.log(obj);
+else {
+    console.log("number is equal");
+}
+
+
+
