48f4f4bad4566d65de23494b7b1a398bc97d6d92
ARUN KUMAR A <86073278+ArunKumarAgit@users.noreply.github.com>
2022-02-06 17:10:23 +0530

    Add files via upload

---
+++ jan25/index.js
@@ -0,0 +1,60 @@
+console.log("hello1");
+
+//print 1 to 10
+
+//var n = 1
+
+//print the first 15 even number
+//2,4,6,.....30.
+
+/*
+var n = 2;
+
+while (n <= 30) {
+  console.log(n);
+  n = n + 2
+}
+
+*/
+
+/*
+var i = prompt("enter the number");
+i = parseInt(i);
+var counter = 0;
+while(counter < i ) {
+    console.log( (2 * counter) + 1);
+    counter = counter + 1;
+}
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
