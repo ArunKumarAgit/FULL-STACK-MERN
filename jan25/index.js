1f2046e8390c4187c8ecead13411d7d5048fa465
ARUN KUMAR A <86073278+ArunKumarAgit@users.noreply.github.com>
2022-02-06 16:55:33 +0530

    Update index.js

--- jan25/index.js
+++ jan25/index.js
@@ -13,6 +13,22 @@ else {
     console.log("number is equal");
 }
 
+//assignment 2:
+//take 3 numbers and write a program to find maximum of those 3 numbers.
+var a = 18;
+var b = 10;
+var c = 56;
+if(a>b && a>c) {
+    console.log("a is maximum");
+} else if(b>a && b>c) {
+    console.log("b is maximum");
+} else {
+    console.log("c is maximum");
+}
 
+output
+------------
+c is maximum
+--------------------------------------
 
 
