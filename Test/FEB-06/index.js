ee35b191389e8080c1a20f6f0a1190ea2faee70b
ARUN KUMAR A <86073278+ArunKumarAgit@users.noreply.github.com>
2022-03-06 20:38:11 +0530

    Add files via upload

---
+++ Test/FEB-06/index.js
@@ -0,0 +1,13 @@
+function popup(n) {
+    var a = document.getElementById(n);
+
+    console.log(n);
+    if(a.classList.contains("details")){
+        a.classList.remove("details");
+        a.classList.add("initial");
+
+    }else {
+        a.classList.remove("initial");
+        a.classList.add("details");
+    }
+}
