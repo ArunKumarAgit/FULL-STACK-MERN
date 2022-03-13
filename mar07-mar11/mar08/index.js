7e2552c76d787f8a7ef27bd318ddd10934847a7b
ARUN KUMAR A <86073278+ArunKumarAgit@users.noreply.github.com>
2022-03-13 06:41:44 +0530

    Add files via upload

---
+++ mar07-mar11/mar08/index.js
@@ -0,0 +1,8 @@
+
+function x(n){
+    var a = document.getElementsByClassName("con")[0];
+    var b = document.getElementsByClassName("dis")[n];
+        a.style.display = "none";
+    b.style.display = "initial";
+   }
+
